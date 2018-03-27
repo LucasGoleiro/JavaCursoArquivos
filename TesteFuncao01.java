0953b5462c1c8906cc3bd9cfb6d9c5124d862126
LucasGoleiro <37819566+LucasGoleiro@users.noreply.github.com>
2018-03-27 14:45:12 -0300

    Add files via upload

---
+++ TesteFuncao01.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testefuncao01;
+
+/**
+ *
+ * @author lucas_santos
+ */
+public class TesteFuncao01 {
+
+    /**O programa abaixo só será executado se for chamado no programa Main.
+     * @param args the command line arguments
+     */
+    
+    static void soma(int a, int b) {
+        int s= a + b;
+        System.out.println("A soma é " + s);
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Começou o programa");
+        soma (5,2);
+    }
+    
+}
